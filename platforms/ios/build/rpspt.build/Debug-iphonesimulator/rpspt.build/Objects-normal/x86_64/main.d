f8d5b3a7e0afdfef5419ce51dd0da50f745c63fe
butcer0 <Erik@ESCAdmins-Mac-mini-2.local>
2017-07-09 05:54:14 -0400

    Init Chechin, stable

---
+++ platforms/ios/build/rpspt.build/Debug-iphonesimulator/rpspt.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/Erik/Projects/rpspt/platforms/ios/rpspt/rpspt-Prefix.pch \
+  /Users/Erik/Projects/rpspt/platforms/ios/internal/main.m \
+  /Users/Erik/Projects/rpspt/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/Erik/Projects/rpspt/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/Erik/Projects/rpspt/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/Erik/Projects/rpspt/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/Erik/Projects/rpspt/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/Erik/Projects/rpspt/platforms/ios/internal/TNSDebugging.h \
+  /Users/Erik/Projects/rpspt/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
